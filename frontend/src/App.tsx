bccc297ad7ecdf524ae1e26ca7869dd754488db4
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-07-24 19:59:10 -0300

    feat: desenvolvimento da estilização do projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,53 @@
+import { FiTrash } from "react-icons/fi";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full min-h-screen bg-gray-800 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
+
+        <form className="flex flex-col my-6">
+          <label className="font-medium text-white">Nome:</label>
+          <input
+            type="text"
+            placeholder="Digite seu nome completo"
+            className="w-full mb-5 p-2 rounded"
+          />
+
+          <label className="font-medium text-white">Email:</label>
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            className="w-full mb-5 p-2 rounded"
+          />
+
+          <input
+            type="submit"
+            value="Cadastrar"
+            className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"
+          />
+        </form>
+
+        <section className="flex flex-col">
+          <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-300">
+            <p>
+              <span className="font-medium">Nome:</span>Matheus
+            </p>
+            <p>
+              <span className="font-medium">Email:</span>teste@teste.com
+            </p>
+            <p>
+              <span className="font-medium">Status:</span>ATIVO
+            </p>
+
+            <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
+              <FiTrash size={18} color="#FFF" />
+            </button>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default App;
